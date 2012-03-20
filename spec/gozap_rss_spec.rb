a0d8aa1c4c66f8cc2b3af4b943166213ebda823f
王明华 <wangmh.bit@gmail.com>
2012-03-20 14:10:29 +0800

    添加了测试样本

--- spec/gozap_rss_spec.rb
+++ spec/gozap_rss_spec.rb
@@ -1,16 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
+require 'csv'
 describe "GozapRss" do
   it "hello" do
-     GozapRss::VERSION.should ==  "0.0.5"
+     GozapRss::VERSION.should ==  "0.0.6"
   end
 
   it "test xml" do
-    rss = GozapRss::ChoutiRss.new("http://www.neihan8.com/data/rss/3.xml")
-    puts rss.items[0].title
-    rss.items.count.should >  1
-    #rss1 =  RSS::Parser.parse("http://hi.baidu.com/cwyalpha/rss", false)
-    #puts rss1.items
+    index = 0
+    CSV.foreach("./chouti_rss.csv") do |row|
+      rss = GozapRss::ChoutiRss.new row[1]
+      if rss.items.nil?
+        puts row[1] + " nil"
+      end
+      index += 1
+      puts  "#{index}: #{row[1]} =>  #{rss.items.count}" if rss.items
+
+    end
+
+    #rss = GozapRss::ChoutiRss.new("http://hi.baidu.com/cwyalpha/rss")
+    ##
+    #puts rss.items[1].title
+    #rss.items.count.should >  1
+    ##rss = RSS::Parser.parse("")
+    ##rss.items.count.should >  1
 
   end
 
